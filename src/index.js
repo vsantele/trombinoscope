ff2c8a7fa9fa546589652c141334dab637314825
Victor Santelé <Vic16@hotmail.be>
2019-03-12 22:40:17 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
 const isDev = require('electron-is-dev')
+const path = require('path')
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
-
-const server = "wolfvic.duckdns.org"
-const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+require('update-electron-app')({
+  repo: "wolfvic/trombinoscope",
+  updateInterval: "1 hour"
+})
 
 // if(!isDev) {
 //   autoUpdater.setFeedURL(feed)
@@ -43,6 +45,7 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png')
   });
 
   // and load the index.html of the app.
